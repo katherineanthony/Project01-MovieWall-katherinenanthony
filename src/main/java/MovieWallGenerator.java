71547330e678e7183739c147eecc4606e4a6a924
katherineanthony <katherinesanthony@gmail.com>
2022-10-17 20:16:29 -0700

    added UserInput features

--- src/main/java/MovieWallGenerator.java
+++ src/main/java/MovieWallGenerator.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MovieWallGenerator {
@@ -13,11 +14,15 @@ public class MovieWallGenerator {
 
     public void searchForActor(String userInput) {
         System.out.println("You said: " + userInput);
+        boolean hasActor = false;
         for (int i = 0; i < actors.size(); i++) {
             if (userInput.equalsIgnoreCase(actors.get(i).getName())) {
                 actors.get(i).getMovies();
+                hasActor = true;
             }
-
+        }
+        if (!hasActor) {
+            System.out.println("No such actor. Did you mean: ");
         }
     }
 
@@ -30,18 +35,15 @@ public class MovieWallGenerator {
             while (line != null) {
                 String[] movieTitle = line.split(",");
                 movies.add(numMovies, movieTitle[1]); // add movie to array at index of movie #
-
-
                 String[] allActors = line.split("\\{");
+
                 for (int j = 0; j < allActors.length; j++) {
                     Movie movie = new Movie(); // does this need to be moved?
                     Actor actor = new Actor(); // is this in the wrong spot?
-                    //System.out.println("one actor: " + allActors[j]);
                     String[] actorComponents = allActors[j].split(",");
 
                     // we're within one actor:
                     for (int i = 0; i < actorComponents.length; i++) {
-
                         if (actorComponents[i].contains("character") || actorComponents[i].contains("job")) {
                             int index = actorComponents[i].indexOf(":");
                             String temp = actorComponents[i].substring(index + 4);
@@ -66,9 +68,10 @@ public class MovieWallGenerator {
                             actor.setName(name);
                         }
                     }
+                    // TODO: check if the actor already exists in the List
                     actors.add(actor);
                 }
-                numMovies++; // we get the correct values of movies
+                numMovies++;
                 try {
                     line = br.readLine();
                 } catch (IOException e) {
@@ -85,97 +88,20 @@ public class MovieWallGenerator {
     public static void main(String[] args) throws FileNotFoundException {
         MovieWallGenerator generator = new MovieWallGenerator();
         //String file = args[0] + "/tmdb_5000_credits.csv";
-
         String file = "/Users/katherineanthony/IdeaProjects/CS245/Project01-MovieWall-katherinenanthony/src/main/resources/tmdb_5000_credits.csv";
         generator.readFile(file);
         System.out.println("Welcome to the Movie Wall!");
-        System.out.println("Enter an actor's name: ");
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an actor's name " +
+                "(or \"EXIT\" to quit): ");
         String userInput = scan.nextLine();
-        generator.searchForActor(userInput);
+        while (!userInput.equalsIgnoreCase("exit")) {
+            generator.searchForActor(userInput);
+            System.out.println("Enter an actor's name " +
+                    "(or \"EXIT\" to quit): ");
+            userInput = scan.nextLine();
+        }
+        System.out.println("Thank you for using the movie wall!");
     }
 }
-
-
-
-        //System.out.println(movies.get(1));
-        //System.out.println(actors.get(1));
-
-        /*for (int i = 0; i < actors.size(); i++) {
-            System.out.println(actors.get(i));
-            actors.get(i).getMovies();
-            //System.out.println(actors.get(i).getMovies());
-        } for (int i = 0; i < actors.size(); i++) {
-            System.out.println("actor: " + actors.get(i));
-            actors.get(i).getMovies();
-       }
-
-        */
-        //for (int i = 0; i < movies.size(); i++)
-        //    System.out.println(movies.get(i));
-        //System.out.println(movies.size());
-        //System.out.println(actors.size());
-
-        /*
-        - I THINK THE PROBLEM IS WITH ADDING THE MOVIE TO A PERSON
-         */
-
-        //System.out.println(actors);
-        /*Scanner scanner = new Scanner(System.in);
-        //System.out.println("Enter an actor's name: ");
-
-        String actor = scanner.nextLine();
-        System.out.println(actor);
-        ArrayList<Movie> toBePrinted = new ArrayList<>();
-        System.out.println(actors.toString());
-        for (int i = 0; i < actors.size(); i++) {
-            if (actors.get(i).getName().compareTo(actor) == 0) {
-                actors.get(i).getMovies(toBePrinted);
-            }
-        }
-        if (toBePrinted.size() != 0) {
-            for (int i = 0; i < actors.size(); i++) {
-                System.out.println(toBePrinted.get(i));
-            }
-        }
-        else {
-            System.out.println("There are no movies with this actor. Did you mean: ");
-        }*/
-        /* TODO: i think there's a problem with the naming of the actor/scope. the using
-                    or lifetime of Actor is not sustainable because it doesn't live throughout
-                    the finding of character and name.
-                    for (int j = 0; j < part.length; j++) {
-                        if (part[j].contains("character") || part[j].contains("job")) {
-                            int index = part[j].indexOf(":");
-                            String temp = part[j].substring(index + 4);
-                            String role;
-                            if (temp.contains("\"")) {
-                                role = temp.substring(0, temp.indexOf("\""));
-                            } else {
-                                role = part[j].substring(index + 4, part[j].length() - 2);
-                            }
-
-                            movie.setTitle(title[1]);
-                            movie.setRole(role);
-                            actor.addMovie(movie); // this works to some extent
-
-                            actor.getMovies();
-                        } else if (part[j].contains("name")) {
-                            int index = part[j].indexOf(":");
-                            String temp = part[j].substring(index + 4, part[j].length() - 2);
-                            String name;
-                            if (temp.contains("\"")) {
-                                name = temp.substring(0, temp.indexOf("\""));
-                            } else {
-                                name = part[j].substring(index + 4, part[j].length() - 2);
-                            }
-                            actor.setName(name);
-                        }
-                        if (actor.getName() != null) {
-                            actors.add(actor);
-                            //actor.getMovies();
-                            actor = null;
-                        }
-                    }*/
-
 
